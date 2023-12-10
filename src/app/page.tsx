8500bc7e6c2d35137721d059e6f2b9d4fdb10ca2
Roy Anger <roy@royanger.com>
2023-12-10 18:54:33 -0500

    Adjusted layout and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { UserButton as ClerkUserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-[calc(100vh-64px)] items-center justify-center">
       <UserButton />
     </div>
   );
